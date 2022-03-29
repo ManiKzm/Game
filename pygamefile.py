9c3ed6b41cc7314410c10c161aedbd906270dbd4
Mani <68550610+ManiKzm@users.noreply.github.com>
2022-03-29 13:45:25 +0430

    added bullet
    shooting bullet feature added

--- pygamefile.py
+++ pygamefile.py
@@ -15,6 +15,16 @@ tank1Ychange = 0
 def place_tank1(x, y):
     wn.blit(tank1, (x, y))
 
+# Create the bullet
+bullet = pygame.image.load("bullet.png")
+
+def place_bullet(x, y):
+    wn.blit(bullet, (x, y))
+#place bullet on screen or not
+bullet_on_screen = False
+bullet_y = 0
+bullet_x = 0
+
 pygame.display.set_caption("Tank Game")# Title
 # Image Icon
 icon = pygame.image.load("tank.png")
@@ -37,6 +47,10 @@ while running:
                 tank1Ychange = -0.5
             elif event.key == pygame.K_DOWN:
                 tank1Ychange = 0.5
+            if event.key == pygame.K_SPACE:
+                bullet_on_screen = True
+                bullet_x = tank1X + 24
+                bullet_y = tank1Y - 5
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                 tank1Xchange = 0
@@ -45,5 +59,8 @@ while running:
         tank1X += tank1Xchange
     tank1Y += tank1Ychange
     place_tank1(tank1X, tank1Y)
+    if bullet_on_screen:
+        place_bullet(bullet_x, bullet_y)
+        bullet_y -= 1
     pygame.display.update()
     time.sleep(0.001)
