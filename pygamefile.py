bdb12f8061e9b427d7a286682c35b61bd41771ba
ShayanBemola <91028264+ShayanBemola@users.noreply.github.com>
2022-03-31 16:29:10 +0430

    Update pygamefile.py
    Added directional bullets

--- pygamefile.py
+++ pygamefile.py
@@ -1,5 +1,3 @@
-from shutil import move
-from termios import CINTR
 import pygame
 
 pygame.init()
@@ -10,7 +8,8 @@ pygame.display.set_icon(icon)
 
 
 WIDTH, HEIGHT = 800, 600
-SPEED = 3
+TANK_SPEED = 3
+BULLET_SPEED = 8
 CLOCK = pygame.time.Clock()
 
 wn = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -22,35 +21,72 @@ tank_image_L = pygame.image.load('tank_L.png').convert_alpha()
 
 tank_image = tank_image_U
 tank = tank_image.get_rect()
+tank_direction = 'U'
 
 
-bullet_image = pygame.image.load("bullet.png").convert_alpha()
+bullet_image_U = pygame.image.load("bullet_U.png").convert_alpha()
+bullet_image_R = pygame.image.load("bullet_R.png").convert_alpha()
+bullet_image_D = pygame.image.load("bullet_D.png").convert_alpha()
+bullet_image_L = pygame.image.load("bullet_L.png").convert_alpha()
+
+bullet_image = bullet_image_U
 bullet = bullet_image.get_rect()
+bullet_fired = False
 
-def place_bullet(x, y):
-    wn.blit(bullet, (x, y))
-bullet_on_screen = False
-bullet_y = 0
-bullet_x = 0
 
-def movement(direction):
-    global tank_image
-    if direction == 'L': # Left
+def tank_movement():
+    global tank_image, bullet_image
+    if tank_direction == 'L': # Left
         tank_image = tank_image_L
-        if tank.left - SPEED >= 0:
-            tank.x -= SPEED
-    elif direction == 'R': # Right
+        if tank.left - TANK_SPEED >= 0:
+            tank.x -= TANK_SPEED
+    elif tank_direction == 'R': # Right
         tank_image = tank_image_R
-        if tank.right + SPEED <= WIDTH:
-            tank.x += SPEED
-    elif direction == 'U': # Up
+        if tank.right + TANK_SPEED <= WIDTH:
+            tank.x += TANK_SPEED
+    elif tank_direction == 'U': # Up
         tank_image = tank_image_U
-        if tank.top - SPEED >= 0:
-            tank.y -= SPEED
+        if tank.top - TANK_SPEED >= 0:
+            tank.y -= TANK_SPEED
     else: # Down
         tank_image = tank_image_D
-        if tank.bottom + SPEED <= HEIGHT:
-            tank.y += SPEED
+        if tank.bottom + TANK_SPEED <= HEIGHT:
+            tank.y += TANK_SPEED
+
+def bullet_firing(direction):
+    global bullet_fired
+    if direction == 'L': # Left
+        bullet_image = bullet_image_L
+        if bullet.left - BULLET_SPEED >= 0:
+            bullet.x -= BULLET_SPEED
+        else:
+            bullet_fired = False
+    elif direction == 'R': # Right
+        bullet_image = bullet_image_R
+        if bullet.right + BULLET_SPEED <= WIDTH:
+            bullet.x += BULLET_SPEED
+        else:
+            bullet_fired = False
+    elif direction == 'U': # Up
+        bullet_image = bullet_image_U
+        if bullet.top - BULLET_SPEED >= 0:
+            bullet.y -= BULLET_SPEED
+        else:
+            bullet_fired = False
+    else: # Down
+        bullet_image = bullet_image_D
+        if bullet.bottom + BULLET_SPEED <= HEIGHT:
+            bullet.y += BULLET_SPEED
+        else:
+            bullet_fired = False
+
+    wn.blit(bullet_image, bullet)
+
+
+        
+
+        
+
 
 running = True
 
@@ -63,21 +99,30 @@ while running:
 
 
     keys = pygame.key.get_pressed()
-    #Movement and Rotation
+    #tank_movement and Rotation
     if keys[pygame.K_a] or keys[pygame.K_LEFT]: # left
-        movement('L')
+        tank_direction = 'L'
+        tank_movement()
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]: # right
-        movement('R')
+        tank_direction = 'R'
+        tank_movement()
     if keys[pygame.K_w] or keys[pygame.K_UP]: # up
-        movement('U')
+        tank_direction = 'U'
+        tank_movement()
     if keys[pygame.K_s] or keys[pygame.K_DOWN]: # down
-        movement('D')
-    
+        tank_direction = 'D'
+        tank_movement()
+    if keys[pygame.K_SPACE]: #bullet
+        if bullet_fired == False:
+            bullet_fired = True
+            bullet_direction = tank_direction
+            bullet.center = tank.center
 
+    
     wn.blit(tank_image, tank)
-        
-    if bullet_on_screen:
-        place_bullet(bullet_x, bullet_y)
-        bullet_y -= 1
+
+    if bullet_fired:
+        bullet_firing(bullet_direction)
+    
     pygame.display.update()
     CLOCK.tick(90)
