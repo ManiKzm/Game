71783dd57016018d91285082577c07966c1a1346
Mani <68550610+ManiKzm@users.noreply.github.com>
2022-03-31 12:28:16 +0430

    enabled arrow keys

--- pygamefile.py
+++ pygamefile.py
@@ -1,34 +1,56 @@
-import pygame, time
+from shutil import move
+from termios import CINTR
+import pygame
 
-# Intialize the pygame
 pygame.init()
+pygame.display.set_caption("Tank Game")
+icon = pygame.image.load("tank.png")
+pygame.display.set_icon(icon)
 
-# Create a window
-wn = pygame.display.set_mode((800,600))
 
-# Creating the tank1
-tank1 = pygame.image.load('tank1.png')
-tank1X = 368
-tank1Y = 500
-tank1Xchange = 0
-tank1Ychange = 0
-def place_tank1(x, y):
-    wn.blit(tank1, (x, y))
 
-# Create the bullet
-bullet = pygame.image.load("bullet.png")
+WIDTH, HEIGHT = 800, 600
+SPEED = 3
+CLOCK = pygame.time.Clock()
+
+wn = pygame.display.set_mode((WIDTH, HEIGHT))
+
+tank_image_U = pygame.image.load('tank_U.png').convert_alpha()
+tank_image_R = pygame.image.load('tank_R.png').convert_alpha()
+tank_image_D = pygame.image.load('tank_D.png').convert_alpha()
+tank_image_L = pygame.image.load('tank_L.png').convert_alpha()
+
+tank_image = tank_image_U
+tank = tank_image.get_rect()
+
+
+bullet_image = pygame.image.load("bullet.png").convert_alpha()
+bullet = bullet_image.get_rect()
 
 def place_bullet(x, y):
     wn.blit(bullet, (x, y))
-#place bullet on screen or not
 bullet_on_screen = False
 bullet_y = 0
 bullet_x = 0
 
-pygame.display.set_caption("Tank Game")# Title
-# Image Icon
-icon = pygame.image.load("tank.png")
-pygame.display.set_icon(icon)
+def movement(direction):
+    global tank_image
+    if direction == 'L': # Left
+        tank_image = tank_image_L
+        if tank.left - SPEED >= 0:
+            tank.x -= SPEED
+    elif direction == 'R': # Right
+        tank_image = tank_image_R
+        if tank.right + SPEED <= WIDTH:
+            tank.x += SPEED
+    elif direction == 'U': # Up
+        tank_image = tank_image_U
+        if tank.top - SPEED >= 0:
+            tank.y -= SPEED
+    else: # Down
+        tank_image = tank_image_D
+        if tank.bottom + SPEED <= HEIGHT:
+            tank.y += SPEED
 
 running = True
 
@@ -38,29 +60,24 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                tank1Xchange = 0.5
-            elif event.key == pygame.K_LEFT:
-                tank1Xchange = -0.5
-            if event.key == pygame.K_UP:
-                tank1Ychange = -0.5
-            elif event.key == pygame.K_DOWN:
-                tank1Ychange = 0.5
-            if event.key == pygame.K_SPACE:
-                bullet_on_screen = True
-                bullet_x = tank1X + 24
-                bullet_y = tank1Y - 5
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
-                tank1Xchange = 0
-                tank1Ychange = 0
-    if not ((tank1X < 0 and tank1Xchange < 0) or (tank1X > 736 and tank1Xchange > 0)):
-        tank1X += tank1Xchange
-    tank1Y += tank1Ychange
-    place_tank1(tank1X, tank1Y)
+
+
+    keys = pygame.key.get_pressed()
+    #Movement and Rotation
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]: # left
+        movement('L')
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]: # right
+        movement('R')
+    if keys[pygame.K_w] or keys[pygame.K_UP]: # up
+        movement('U')
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]: # down
+        movement('D')
+    
+
+    wn.blit(tank_image, tank)
+        
     if bullet_on_screen:
         place_bullet(bullet_x, bullet_y)
         bullet_y -= 1
     pygame.display.update()
-    time.sleep(0.001)
+    CLOCK.tick(90)
